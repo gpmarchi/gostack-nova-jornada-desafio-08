fd1e41ae3fd6218333c9ff9b23d4415b96f091c5
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-09-18 16:17:22 -0300

    feat: product registration

--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -25,7 +25,7 @@ describe('App', () => {
   beforeEach(async () => {
     // await connection.query('DELETE FROM orders_products');
     // await connection.query('DELETE FROM orders');
-    // await connection.query('DELETE FROM products');
+    await connection.query('DELETE FROM products');
     await connection.query('DELETE FROM customers');
   });
 
