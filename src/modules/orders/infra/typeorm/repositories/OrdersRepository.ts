2e15ad1bc67a8e677c0fe75d91e5bcde99df3b7f
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-09-19 20:28:10 -0300

    feat: show order with purchased products

--- src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
+++ src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
@@ -24,7 +24,7 @@ class OrdersRepository implements IOrdersRepository {
 
   public async findById(id: string): Promise<Order | undefined> {
     const order = await this.ormRepository.findOne(id, {
-      relations: ['orders_products', 'customers'],
+      relations: ['order_products', 'customer'],
     });
 
     return order;
